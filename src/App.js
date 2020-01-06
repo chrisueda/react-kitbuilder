fbb467368bfb46f8687affb648e39deb3dc34b47
Christopher Ueda <chrisu@benefitcosmetics.com>
2020-01-06 11:40:54 -0800

    basic kitbuilder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import KitList from "./KitList";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import Order from "./Order";
+import { loadKits } from "./lib/kitService";
+import { findById } from "./lib/kitHelpers";
+
+class App extends React.Component {
+  state = {
+    kits: [],
+    order: {},
+    currentKit: null,
+    currentCategory: 0
+  };
+
+  componentDidMount() {
+    loadKits().then(kits => this.setState({ kits }));
+  }
+
+  chooseKit = id => {
+    this.setState({ currentKit: id, currentCategory: 0, order: {} });
+  };
+
+  chooseCategory = idx => {
+    this.setState({ currentCategory: idx });
+  };
+
+  addToOrder = (category, id) => {
+    const order = { ...this.state.order };
+    order[category] = id;
+    this.setState({ order });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Kitbuilder</h1>
+        </header>
+        <main>
+          <KitList kits={this.state.kits} chooseKit={this.chooseKit} />
+          <CategoryList
+            kit={this.state.kits[this.state.currentKit]}
+            chooseCategory={this.chooseCategory}
+            currentCategory={this.state.currentCategory}
+          />
+          {this.state.currentKit && (
+            <ProductList
+              currentCategory={this.state.currentCategory}
+              category={
+                this.state.kits[this.state.currentKit].categories[
+                  this.state.currentCategory
+                ]
+              }
+              addToOrder={this.addToOrder}
+            />
+          )}
+          {Object.keys(this.state.order).length !== 0 && (
+            <Order
+              order={this.state.order}
+              kit={
+                this.state.currentKit
+                  ? this.state.kits[this.state.currentKit]
+                  : ""
+              }
+            />
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
